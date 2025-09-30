aa6d7dc99211ca629fab09fa03329473713842c5
narendra1919 <narendrachodisetti2@gmail.com>
2025-09-30 23:56:13 +0530

    object.py

--- object.py
+++ object.py
@@ -2,8 +2,8 @@ import cv2
 
 cap = cv2.VideoCapture("video.mp4")
 
-line_up, = 600,  #if obj cross this line upwards then count
-line_down=600     ##if obj cross this line downwards then count
+line_up, = 600, 
+line_down=600    
 min_size=80
 algo = cv2.createBackgroundSubtractorMOG2()
 
@@ -23,18 +23,18 @@ while True:
     for cnt in contours:
         x,y,w,h = cv2.boundingRect(cnt)
         if w >= min_size and h >= min_size:
-            cur.append((x, y))                          #detect top-left corner
-            cv2.rectangle(f, (x,y), (x+w,y+h), (0,255,0), 2) #mark rectangle for detected obj
+            cur.append((x, y))                          
+            cv2.rectangle(f, (x,y), (x+w,y+h), (0,255,0), 2) 
     for i in cur:
         for j in prev:
-            if abs(i[0]-j[0]) < 30:               #check the distance b/w I&J  is less than 30 units or not
-                if j[1] < line_up <= i[1]:        #if j less than up line than it count DOWN
+            if abs(i[0]-j[0]) < 30:            
+                if j[1] < line_up <= i[1]:     
                     down_count += 1
-                elif j[1] > line_down >= i[1]:    #else UP count "if we use else than it count contiusly"
+                elif j[1] > line_down >= i[1]:  
                     up_count += 1
     prev = cur
     cv2.line(f, (0, line_up), (f.shape[1], line_up), (255,0,0), 2)
-    cv2.line(f, (0, line_down), (f.shape[1], line_down), (0,255,0), 2)   #remove this lines to hide count line(optinal)
+    cv2.line(f, (0, line_down), (f.shape[1], line_down), (0,255,0), 2)  
 
     cv2.putText(f, f'Down:{down_count}', (50,40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
     cv2.putText(f, f'Up:{up_count}', (50,80), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
@@ -43,7 +43,7 @@ while True:
 
     key=cv2.waitKey(30) & 0xFF
 
-    if key == 32:    #ASII VALUE OF SPACE IS 32 used to pause/continue
+    if key == 32:   
         while cv2.waitKey(30) != 32:
             pass
     elif cv2.waitKey(30) & 0xFF == ord('p'):
@@ -53,5 +53,6 @@ print(f"up:{up_count}; down:{down_count}")
 
 cap.release()
 cv2.destroyAllWindows()
+
 
 ##
